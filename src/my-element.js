48d1104c26f359dfa60bb7cb1c7f7b63be00f75d
Jouni Männistö <jouni.mannisto@gmail.com>
2023-04-04 14:05:11 +0300

    highcharts and build options

--- src/my-element.js
+++ src/my-element.js
@@ -1,6 +1,10 @@
-import { LitElement, css, html } from 'lit'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
+import { LitElement, css, html } from "lit";
+import "highcharts";
+import "highcharts-exporting";
+import "./bar-chart";
+
+import litLogo from "./assets/lit.svg";
+import viteLogo from "/vite.svg";
 
 /**
  * An example element.
@@ -20,37 +24,25 @@ export class MyElement extends LitElement {
        * The number of times the button has been clicked.
        */
       count: { type: Number },
-    }
+    };
   }
 
   constructor() {
-    super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
+    super();
+    this.docsHint = "Click on the Vite and Lit logos to learn more";
+    this.count = 0;
   }
 
   render() {
     return html`
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
+        <bar-chart></bar-chart>
       </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
-    `
+    `;
   }
 
   _onClick() {
-    this.count++
+    this.count++;
   }
 
   static get styles() {
@@ -124,8 +116,8 @@ export class MyElement extends LitElement {
           background-color: #f9f9f9;
         }
       }
-    `
+    `;
   }
 }
 
-window.customElements.define('my-element', MyElement)
+window.customElements.define("my-element", MyElement);
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-element.js',
-      formats: ['es'],
+      entry: "index.html",
+      formats: ["es"],
     },
     rollupOptions: {
-      external: /^lit/,
+      // external: /^lit/,
     },
   },
-})
+});
